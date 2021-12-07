b7d2e361c76a02bd872a70e0ea93e1b3c2ad7583
Omar Miatello <jackl85@gmail.com>
2021-12-07 18:37:20 +0100

    add benchmarks

--- src/AdventOfCode.kt
+++ src/AdventOfCode.kt
@@ -1,4 +1,8 @@
+@file:OptIn(ExperimentalTime::class)
+
 import java.io.File
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
 
 fun <PARSED, RES1, RES2> adventOfCode(
     day: Int,
@@ -11,32 +15,23 @@ fun <PARSED, RES1, RES2> adventOfCode(
     filenamePart2: String = "Day${day}",
 ) {
     println("Day $day")
-
-    on(filename = "${filenamePart1}_test") { input ->
-        val output = part1(parser(input))
-        println("Part 1 (test) - expected value: $expectedTest1")
-        check(output == expectedTest1) { "Something goes wrong, current result: $output" }
-        output
-    }
-
-    on(filename = filenamePart1) { input -> part1(parser(input)) }
-
-    on(filename = "${filenamePart2}_test") { input ->
-        val result = part2(parser(input))
-        println("Part 2 (test) - expected value: $expectedTest2")
-        check(result == expectedTest2) { "Something goes wrong, current result: $result" }
-        result
-    }
-
-    on(filename = filenamePart2) { input -> part2(parser(input)) }
+    on(filename = "${filenamePart1}_test", expected = expectedTest1) { part1(parser(it)) }
+    on(filename = filenamePart1) { part1(parser(it)) }
+    on(filename = "${filenamePart2}_test", expected = expectedTest2) { part2(parser(it)) }
+    on(filename = filenamePart2) { part2(parser(it)) }
 }
 
 private fun <RES> on(
     filename: String,
+    expected: RES? = null,
     block: (List<String>) -> RES,
 ) {
     val input = File("data", "$filename.txt").readLines()
-    val output = block(input)
-    println("$filename output: $output")
-    File("data", "${filename}_output.txt").writeText(output.toString())
+    val (output, duration) = measureTimedValue { block(input) }
+    val millis = duration.inWholeMilliseconds
+    if (expected != null) {
+        check(output == expected) { "[$millis ms] Something goes wrong! expected: $expected | current: $output" }
+    } else {
+        println("[${millis}ms]\t$output")
+    }
 }
