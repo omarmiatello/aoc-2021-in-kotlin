3a2ce9b7f602aac7bc42a791055cff7460ee6607
Omar Miatello <jackl85@gmail.com>
2021-12-09 13:03:28 +0100

    day9

---
+++ src/Day09.kt
@@ -0,0 +1,32 @@
+// https://adventofcode.com/2021/day/9
+object Day09 : AdventOfCode, Solution by Omar_Miatello(
+    day = 9,
+    parser = { lines -> lines.map { line -> line.map { "$it".toInt() } } },
+    part1 = { input ->
+        fun getOrMax(x: Int, y: Int) = input.getOrNull(y)?.getOrNull(x) ?: Int.MAX_VALUE
+
+        input.flatMapIndexed { y, row ->
+            row.filterIndexed { x, n ->
+                listOf(x + 1 to y, x - 1 to y, x to y + 1, x to y - 1)
+                    .map { (x1, y1) -> getOrMax(x1, y1) }
+                    .all { n < it }
+            }.map { it + 1 }
+        }.sum()
+    },
+    expectedTestPart1 = 15,
+    part2 = { input ->
+        val booleans = input.map { it.map { it != 9 }.toMutableList() }
+        fun getOrFalse(x: Int, y: Int) = booleans.getOrNull(y)?.getOrNull(x) ?: false
+        fun findSize(x: Int, y: Int): Int = if (getOrFalse(x, y)) {
+            booleans[y][x] = false
+            1 + listOf(x + 1 to y, x - 1 to y, x to y + 1, x to y - 1)
+                .sumOf { (x1, y1) -> findSize(x1, y1) }
+        } else 0
+
+        val basinSizes = booleans.flatMapIndexed { y, row ->
+            row.indices.map { x -> findSize(x, y) }
+        }
+        basinSizes.sortedDescending().take(3).reduce { acc, n -> acc * n }
+    },
+    expectedTestPart2 = 1134,
+)
